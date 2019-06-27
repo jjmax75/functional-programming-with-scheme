295abaff932e3d61ae9ad4b281e1580148d21795
John Behan <jbehan@mywebgrocer.com>
2019-06-27 14:55:56 +0100

    splice a string - 1.3

---
+++ splice-string.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname splice-string) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; constants
+(define str "helloworld")
+(define i 5)
+
+; functions
+(define start (substring str 0 i))
+
+(define end (substring str i))
+
+(define insert-underscore  
+  (string-append start "_" end))
+
+insert-underscore
