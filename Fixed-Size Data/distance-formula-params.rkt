eabac33282bd705b921a97927b3ca7dd7fd3deb6
John Behan <behanj@gmail.com>
2019-06-29 10:54:48 +0100

    correct naming

--- Fixed-Size Data/distance-formula-params.rkt
+++ Fixed-Size Data/distance-formula-params.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname distance-formula-params) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; function expression
+; function definition
 (define (distance-to-origin x y)
   (sqrt (+ (expt x 2) (expt y 2))))
 
