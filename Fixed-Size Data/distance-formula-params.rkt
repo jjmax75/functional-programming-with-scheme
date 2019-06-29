9d56e7c3476afb4116c77f93008fcee0b86c9f3a
John Behan <behanj@gmail.com>
2019-06-29 10:53:55 +0100

    distance calc - 2.1

---
+++ Fixed-Size Data/distance-formula-params.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname distance-formula-params) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; function expression
+(define (distance-to-origin x y)
+  (sqrt (+ (expt x 2) (expt y 2))))
+
+; function application
+(distance-to-origin 5 12)
